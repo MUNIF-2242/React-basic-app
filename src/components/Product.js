51e8cd25b0619ed4028657ab87d446d5f93404d6
munifhasan <munifhasan@dataemb.com>
2023-01-29 17:39:08 +0600

    test

---
+++ src/components/Product.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+class Product extends React.Component {
+  render() {
+    return <div>{/* ... todo ... */}Render from product component</div>
+  }
+}
+export default Product
---
+++ src/components/ProductList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Product from './Product'
+
+class ProductList extends React.Component {
+  render() {
+    return <Product />
+  }
+}
+
+export default ProductList
